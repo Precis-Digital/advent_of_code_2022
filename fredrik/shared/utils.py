538df94ccf072dc0f937c11e7410a338f2cb2f67
Fredrik Nilsson <fredrik.nilsson@precisdigital.com>
2022-12-01 12:59:34 +0100

    update structure

--- fredrik/shared/utils.py
+++ fredrik/shared/utils.py
@@ -1,3 +1,11 @@
+import inspect
+
+
 def read_input_to_string() -> str | list[str]:
-    with open("input.txt", "r") as file:
+    day = inspect.stack()[1].filename.split("/")[-1].split(".")[0]
+    with open(f"inputs/{day}.txt", "r") as file:
         return file.read()
+
+
+def get_day() -> str:
+    return __file__.split("/")[-1].split(".")[0]
