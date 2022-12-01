ece9472acb69c3322a580ab808c6d868c9e2c0e0
Malik Woods <malik@precisdigital.com>
2022-12-01 13:43:19 +0100

    mw: solution day 1

---
+++ malik/day1.py
@@ -0,0 +1,18 @@
+PATH = "/Users/malikwoods/Developer/advent_of_code_2022/_inputs/day-1-input.txt"
+
+from collections import defaultdict
+
+elf_id = 0
+calorie_dict = defaultdict(list)
+
+
+with open(PATH, 'r') as f:
+    for line in f.readlines():
+        line = line.strip()
+        if line == '':
+            elf_id += 1
+            continue
+        calorie_dict[elf_id].append(int(line))
+
+print('ans1', max([sum(cal_list) for cal_list in calorie_dict.values()]))
+print('ans2', sum(sorted([sum(cal_list) for cal_list in calorie_dict.values()])[-3:]))
