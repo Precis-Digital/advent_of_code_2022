6aa78ee08b595ef96186b7e123de4cde405ee1ed
Fredrik Nilsson <fredrik.nilsson@precisdigital.com>
2022-12-01 13:00:39 +0100

    remove unused

--- fredrik/shared/utils.py
+++ fredrik/shared/utils.py
@@ -5,7 +5,3 @@ def read_input_to_string() -> str | list[str]:
     day = inspect.stack()[1].filename.split("/")[-1].split(".")[0]
     with open(f"inputs/{day}.txt", "r") as file:
         return file.read()
-
-
-def get_day() -> str:
-    return __file__.split("/")[-1].split(".")[0]
