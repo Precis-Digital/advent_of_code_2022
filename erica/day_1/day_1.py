375940c3eac0a3b8b3f6ec8cff55f7c1d528ea6b
ericapetersson <erica@precisdigital.com>
2022-12-01 10:17:48 +0100

    make one file

--- erica/day_1/day_1.py
+++ erica/day_1/day_1.py
@@ -1,5 +1,7 @@
 import pathlib
 from itertools import groupby
+import heapq
+
 
 text = pathlib.Path("erica/day_1/input_data.txt").read_text().split('\n')
 
@@ -11,4 +13,7 @@ def split_condition(value):
 elfs_kcal = [list(v) for k, v in groupby(text, key=split_condition) if not k]
 elf_dict = {elf+1: sum(list(map(int, kcal))) for elf, kcal in enumerate(elfs_kcal)}
 
-print(elf_dict[max(elf_dict, key=elf_dict.get)])
+print(elf_dict[max(elf_dict, key=elf_dict.get)])  # part 1
+
+top_elfs = heapq.nlargest(3, elf_dict, key=elf_dict.get)
+print(sum([elf_dict[top_elf] for top_elf in top_elfs]))  # part 2
