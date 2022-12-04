d26b8e897b352fa60db419eaa2c3f93402a0ff21
Jonas Bæk Precis <jonas@precisdigital.com>
2022-12-04 11:06:02 +0100

    typing

--- jonas/Dec04/main.py
+++ jonas/Dec04/main.py
@@ -4,29 +4,29 @@ def get_assignments_from_file(file_name: str) -> list[list]:
 
     return [parse_assignment(assignment.strip()) for assignment in assignments]
 
-def parse_assignment(assignment: str):
+def parse_assignment(assignment: str) -> list[tuple[set,set]]:
     assignments = assignment.split(",")
 
-    def get_range_set(assignment_range: str):
+    def get_range_set(assignment_range: str) -> set:
         bounds = assignment_range.split("-")
         return set(range(int(bounds[0]),int(bounds[1])+1))
 
     return get_range_set(assignments[0]), get_range_set(assignments[1])
 
-def ranges_encompass_one_another(ranges: tuple) -> int:
+def ranges_encompass_one_another(ranges: tuple[set, set]) -> int:
     return int(ranges[0] <= ranges[1] or ranges[1] <= ranges[0])
 
-def ranges_overlap(ranges: tuple) -> int:
+def ranges_overlap(ranges: tuple[set, set]) -> int:
     return int(len(ranges[0] & ranges[1]) > 0)
 
-# Sample 1
+# Sample 1 - 2
 print(sum([ranges_encompass_one_another(assignment) for assignment in get_assignments_from_file("sample_input.txt")]))
 
-# Part 1
+# Part 1 - 595
 print(sum([ranges_encompass_one_another(assignment) for assignment in get_assignments_from_file("input.txt")]))
 
-# Sample 2
+# Sample 2 - 4
 print(sum([ranges_overlap(assignment) for assignment in get_assignments_from_file("sample_input.txt")]))
 
-# Part 2
+# Part 2 - 952
 print(sum([ranges_overlap(assignment) for assignment in get_assignments_from_file("input.txt")]))
