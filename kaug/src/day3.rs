66756e2ee6774da0a7e759e37b366a007fd4fdc9
pd-kaugesaar <kaugesaar@precisdigital.com>
2022-12-05 07:57:12 +0100

    just playing around with rust for the first time

--- kaug/src/day3.rs
+++ kaug/src/day3.rs
@@ -1,8 +1,7 @@
-use std::{fs, str::Lines};
+use std::fs;
 use std::str::{self, Chars};
 
-fn question_one(input: &str) -> i32 {
-	let rucksacks: Lines = input.lines();
+fn question_one(rucksacks: &Vec<&str>) -> i32 {
 	let mut sum: i32 = 0;
 	for rucksack in rucksacks {
 		let (a, b) = rucksack.split_at(rucksack.len() / 2);
@@ -14,8 +13,7 @@ fn question_one(input: &str) -> i32 {
 	return sum;
 }
 
-fn question_two(input: &str) -> i32 {
-	let rucksacks: Vec<&str> = input.lines().collect();
+fn question_two(rucksacks: &Vec<&str>) -> i32 {
 	let mut sum: i32 = 0;
 	for rucksack in rucksacks.chunks(3) {
 		for c in rucksack[0].chars() {
@@ -36,8 +34,8 @@ fn score(item: char) -> i32 {
 	}
 }
 
-pub fn main() {		
-	let day_two_content: String = fs::read_to_string("./input/day3.txt").unwrap();
-	println!("Day 3 | Question 1: {}", question_one(&day_two_content));
-	println!("Day 3 | Question 2: {}", question_two(&day_two_content));
+pub fn main() -> (i32, i32) {		
+	let input: String = fs::read_to_string("./input/day3.txt").unwrap();
+	let rucksacks: Vec<&str> = input.lines().collect();
+	(question_one(&rucksacks), question_two(&rucksacks))
 }
--- kaug/src/day4.rs
+++ kaug/src/day4.rs
@@ -1,45 +1,34 @@
-use std::{fs, str::Lines};
+use std::fs;
 
 fn parse_pair(line: &str) -> ((i32, i32), (i32, i32)) {
-    let assignments: Vec<&str> = line.split(",").collect();
-    let a: (i32, i32) = parse_range(assignments[0]);
-    let b: (i32, i32) = parse_range(assignments[1]);
-    (a, b)
+	let assignments: Vec<&str> = line.split(",").collect();
+	(parse_range(assignments[0]), parse_range(assignments[1]))
 }
 
 fn parse_range(assignment: &str) -> (i32, i32) {
-    let range: Vec<&str> = assignment.split("-").collect();
-    let low: i32 = range[0].parse::<i32>().unwrap();
-    let high: i32 = range[1].parse::<i32>().unwrap();
-    (low, high)
+	let range: Vec<&str> = assignment.split("-").collect();
+	(range[0].parse::<i32>().unwrap(), range[1].parse::<i32>().unwrap())
 }
 
 fn fully_contains(a: (i32, i32), b: (i32, i32)) -> bool {
-    let (a_low, a_high) = a;
-    let (b_low, b_high) = b;
-    (a_low <= b_low && a_high  >= b_high) || (b_low <= a_low && b_high >= a_high)
+	(a.0 <= b.0 && a.1  >= b.1) || (b.0 <= a.0 && b.1 >= a.1)
 }
 
 fn overlaps(a: (i32, i32), b: (i32, i32)) -> bool {
-    let (a_low, a_high) = a;
-    let (b_low, b_high) = b;
-    (a_low <= b_low && b_low <= a_high) || (b_low <= a_low && a_low <= b_high)
+	(a.0 <= b.0 && b.0 <= a.1) || (b.0 <= a.0 && a.0 <= b.1)
 }
 
-fn question_one(input: &str) -> i32 {
-    let lines: Lines = input.lines();
-    let pairs: Vec<((i32, i32), (i32, i32))> = lines.into_iter().map(|line| parse_pair(line)).collect();
-    pairs.iter().filter(|&pair| fully_contains(pair.0, pair.1)).count() as i32
+fn question_one(pairs: Vec<((i32, i32), (i32, i32))>) -> i32 {
+	pairs.iter().filter(|&pair| fully_contains(pair.0, pair.1)).count() as i32
 }
 
-fn question_two(input: &str) -> i32 {
-    let lines: Lines = input.lines();
-    let pairs: Vec<((i32, i32), (i32, i32))> = lines.into_iter().map(|line| parse_pair(line)).collect();
-    pairs.iter().filter(|&pair| overlaps(pair.0, pair.1)).count() as i32
+fn question_two(pairs: Vec<((i32, i32), (i32, i32))>) -> i32 {
+	pairs.iter().filter(|&pair| overlaps(pair.0, pair.1)).count() as i32
 }
 
-pub fn main() {
-    let input: String = fs::read_to_string("./input/day4.txt").unwrap();
-    println!("Day 4 | Question 1: {}", question_one(&input));
-    println!("Day 4 | Question 2: {}", question_two(&input));
+pub fn main() -> (i32, i32) {
+	let input: String = fs::read_to_string("./input/day4.txt").unwrap();
+	let lines: Vec<&str> = input.lines().collect();
+	let pairs: Vec<((i32, i32), (i32, i32))> = lines.iter().map(|line| parse_pair(line)).collect();
+	(question_one(pairs.clone()), question_two(pairs))
 }
