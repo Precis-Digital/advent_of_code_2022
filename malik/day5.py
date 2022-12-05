bb566b6409a7abb8639d85d49b77bf9728bec37f
Malik Woods <malik@precisdigital.com>
2022-12-05 12:31:29 +0100

    code cleaning

--- malik/day5.py
+++ malik/day5.py
@@ -1,4 +1,5 @@
 import os
+import copy
 import re
 from collections import defaultdict
 
@@ -18,33 +19,36 @@ def get_crate_array(line: str) -> list[str]:
     ]
 
 
+def extract_stacks(lines: list[str]) -> defaultdict[list]:
+    stacks = defaultdict(list)
+    for line in lines:
+        crate_array = get_crate_array(line=line)
+        for i, crate in enumerate(crate_array):
+            if crate != "":
+                stacks[i + 1].append(crate)
+
+    # reverse the order of the crates so the top of the stack is the last element
+    # this all treats the stacks as stacks, not queues!!
+    for k, v in stacks.items():
+        stacks[k] = v[::-1]
+    return stacks
+
+
+def extract_moves(lines: list[str]) -> list[list[int, int, int]]:
+    moves = []
+    for line in lines:
+        m = re.match(r"move (\d+) from (\d+) to (\d+)", line)
+        if m:
+            moves.append(list(map(int, m.groups())))
+    return moves
+
+
 def get_data(file_name: str) -> (defaultdict[list], list[list[int, int, int]]):
     with open(file_name) as f:
-
-        moves = []
-        stacks = defaultdict(list)
-        num_stacks = 0
         lines = f.readlines()
-        last_line = 0
-        for i, line in enumerate(lines):
-            last_line = i
-            if line.startswith(" 1 "):
-                break
-            crate_array = get_crate_array(line=line)
-
-            for i, crate in enumerate(crate_array):
-                if crate != "":
-                    stacks[i + 1].append(crate)
-                    num_stacks = max(num_stacks, i + 1)
-
-        # reverse the order of the crates so the top of the stack is the last element
-        for k, v in stacks.items():
-            stacks[k] = v[::-1]
-
-        for line in lines[last_line:]:
-            m = re.match(r"move (\d+) from (\d+) to (\d+)", line)
-            if m:
-                moves.append(list(map(int, m.groups())))
+        lines_with_crates = [line for line in lines if "[" in line]
+        stacks = extract_stacks(lines=lines_with_crates)
+        moves = extract_moves(lines=lines)
 
         return stacks, moves
 
@@ -52,7 +56,8 @@ def get_data(file_name: str) -> (defaultdict[list], list[list[int, int, int]]):
 def solution(
     stacks: defaultdict[list], moves: list[list[int, int, int]], reverse_order=True
 ) -> str:
-
+    # done so it doesnt mutate the original
+    stacks = copy.deepcopy(stacks)
     step = -1 if reverse_order else 1
     for qty, from_stack, to_stack in moves:
         print(qty, from_stack, to_stack, stacks[from_stack][-qty:])
@@ -63,8 +68,12 @@ def solution(
 
 
 if __name__ == "__main__":
-    stacks, moves = get_data(file_name="inputs/day-5-input.txt")
-    print("solution 1:", solution(stacks=stacks, moves=moves, reverse_order=True))
-
-    stacks, moves = get_data(file_name="inputs/day-5-input.txt")
-    print("solution 2:", solution(stacks=stacks, moves=moves, reverse_order=False))
+    stacks, moves = get_data(file_name="inputs/day-5-sample.txt")
+    print(
+        "solution 1:",
+        solution(stacks=stacks, moves=moves, reverse_order=True),
+    )
+    print(
+        "solution 2:",
+        solution(stacks=stacks, moves=moves, reverse_order=False),
+    )
