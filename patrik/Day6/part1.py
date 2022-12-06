84e7d994db73f6f8bf153960d92c03a48eeb87ad
Patrik Janson <patrik@patrikjanson.com>
2022-12-06 11:58:36 +0100

    Day 6 refactoring

--- patrik/Day6/part1.py
+++
@@ -1,7 +0,0 @@
-stream = open('data.txt', 'r').read()
-
-for i, char in enumerate(stream):
-    if i > 2:
-        if len([*set([stream[i-3], stream[i-2], stream[i-1], char])]) > 3:
-            print(i+1)
-            break
--- patrik/Day6/part2.py
+++
@@ -1,8 +0,0 @@
-stream = open('data.txt', 'r').read()
-
-for i, char in enumerate(stream):
-    if i > 13:
-        arr = [stream[i-j] for j in range(14)]
-        if len([*set(arr)]) > 13:
-            print(i+1)
-            break
