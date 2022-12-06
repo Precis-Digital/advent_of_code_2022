92351cfdcbb826e89bc99b9e2bbe7859a2017073
Jonas Bæk Precis <jonas@precisdigital.com>
2022-12-06 07:35:09 +0100

    clean up

--- jonas/Dec06/main.py
+++ jonas/Dec06/main.py
@@ -1,14 +1,14 @@
-MARKER_TYPE_CHARACTERS = {
+MARKER_TYPE_CHARACTER_NUM = {
     "packet": 4,
     "message": 14
 }
 
 def get_datastream_input(filename: str) -> str:
     with open(filename, "r") as f:
-        return [char for char in f.readline()]
+        return list(f.readline())
 
 def determine_marker_index(datastream: list[str], marker_type: str) -> int:
-    char_nums = MARKER_TYPE_CHARACTERS[marker_type]
+    char_nums = MARKER_TYPE_CHARACTER_NUM[marker_type]
 
     prev_stack = datastream[:char_nums]
     for i, char in enumerate(datastream[char_nums:], char_nums):
@@ -24,11 +24,11 @@ def unique_values(input_list: list[str]) -> list[str]:
 # Sample 1 - 7
 print(determine_marker_index(get_datastream_input("sample_input.txt"), "packet"))
 
-# Part 1
+# Part 1 - 1655
 print(determine_marker_index(get_datastream_input("input.txt"), "packet"))
 
 # Sample 2 - 7
 print(determine_marker_index(get_datastream_input("sample_input.txt"), "message"))
 
-# Part 2
+# Part 2 - 2665
 print(determine_marker_index(get_datastream_input("input.txt"), "message"))
