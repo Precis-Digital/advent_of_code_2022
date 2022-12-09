99e4a3bd07e6e20b99bb46a77ab255059d55ad6e
ericapetersson <erica@precisdigital.com>
2022-12-09 12:44:00 +0100

    day 9

--- erica/day_9/day_9.py
+++ erica/day_9/day_9.py
@@ -31,9 +31,6 @@ def generate_head_coordinates(instructions: list[str]) -> list[tuple[int, int]]:
 
 
 def generate_tail_coordinates(head_coord: list[tuple[int, int]], step=0) -> list[tuple[int, int]]:
-    """Tail coordinates follow the head coordinates, and will be in a coordinate
-     touching the head coordinate even diagonally."""
-    tail_coordinates = [(1, 1)]
     for i, head in enumerate(head_coord):
         last_position = tail_coordinates[-1]
         if i <= step:
