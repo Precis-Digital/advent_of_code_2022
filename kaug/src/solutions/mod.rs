1f36dac794f1da5c8ddb396ad53267e6b1c074f5
pd-kaugesaar <kaugesaar@precisdigital.com>
2022-12-10 14:17:33 +0100

    day9 🪢

--- kaug/src/solutions/mod.rs
+++ kaug/src/solutions/mod.rs
@@ -8,18 +8,20 @@ mod day5;
 mod day6;
 mod day7;
 mod day8;
+mod day9;
 
 
 pub fn get(day: &str) -> &dyn Solution {
-  match day {
-      "day1" => &day1::Day1,
-      "day2" => &day2::Day2,
-      "day3" => &day3::Day3,
-      "day4" => &day4::Day4,
-      "day5" => &day5::Day5,
-      "day6" => &day6::Day6,
-      "day7" => &day7::Day7,
-      "day8" => &day8::Day8,
-      _=> panic!("Invalid day")
-  }
+	match day {
+		"day1" => &day1::Day1,
+		"day2" => &day2::Day2,
+		"day3" => &day3::Day3,
+		"day4" => &day4::Day4,
+		"day5" => &day5::Day5,
+		"day6" => &day6::Day6,
+		"day7" => &day7::Day7,
+		"day8" => &day8::Day8,
+		"day9" => &day9::Day9,
+		_=> panic!("Invalid day")
+	}
 }
