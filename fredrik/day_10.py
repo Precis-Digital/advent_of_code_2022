2453e8b90eaf4c4fa758bf76226dc34863da9257
Fredrik Nilsson <fredrik.nilsson@precisdigital.com>
2022-12-10 19:01:51 +0100

    add missing typehints

--- fredrik/day_10.py
+++ fredrik/day_10.py
@@ -7,36 +7,39 @@ Program = list[str]
 
 
 class CPU:
-    def __init__(self):
+    def __init__(self) -> None:
         self.x_register = 1
         self.cycle = 0
         self.signal_stengths = []
         self.pixels = []
 
-    def addx(self, v: int):
+    def addx(self, v: int) -> None:
         for _ in range(2):
             self.increment_cycle()
         self.x_register += v
 
-    def noop(self):
+    def noop(self) -> None:
         self.increment_cycle()
 
-    def save_signal_strength(self):
+    def save_signal_strength(self) -> None:
         self.signal_stengths.append(self.cycle * self.x_register)
 
-    def draw(self):
-        if abs(self.x_register - self.cycle % CRT_WIDTH) <= 1:
+    def crt_draws_on_sprite(self) -> bool:
+        return abs(self.x_register - self.cycle % CRT_WIDTH) <= 1
+
+    def draw(self) -> None:
+        if self.crt_draws_on_sprite():
             self.pixels.append("#")
         else:
             self.pixels.append(".")
 
-    def increment_cycle(self):
+    def increment_cycle(self) -> None:
         self.draw()
         self.cycle += 1
         if self.cycle in CYCLES_TO_SAVE:
             self.save_signal_strength()
 
-    def run(self, program: Program):
+    def run(self, program: Program) -> None:
         for instruction in program:
             if instruction == "noop":
                 self.noop()
@@ -44,7 +47,7 @@ class CPU:
                 v = int(instruction.split()[-1])
                 self.addx(v=v)
 
-    def render_graphical_output(self):
+    def render_graphical_output(self) -> None:
         for row in utils.chunks(lst=self.pixels, n=CRT_WIDTH):
             print("".join(row))
 
