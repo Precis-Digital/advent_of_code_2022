61af360a32e8bae65fca0525518b93ad35aba314
Malik Woods <malik@precisdigital.com>
2022-12-12 00:01:12 +0100

    Update day10.py

--- malik/day10.py
+++ malik/day10.py
@@ -40,13 +40,40 @@ def solution1(program_output: list[tuple[int, int]]) -> int:
         total += cycle_time * nearest[1]
     return total
 
+def solution2(program: list[tuple[str, int]]):
+    i = 1
+    sprite_position = 1
+    crt = ""
+    crt_pointer = 0
+    while program:
+        instruction, value = program.pop(0)
+        print(instruction, value)
+        ts = 2 if instruction == "addx" else 1
+
+        for _ in range(ts):
+            if crt_pointer in (sprite_position - 1, sprite_position, sprite_position + 1):
+                crt += "#"
+            else:
+                crt += "."
+            crt_pointer += 1
+            crt_pointer = crt_pointer % 40
+        sprite_position += value
+
+    for i in range(0, 240, 40):
+        print(crt[i : i + 40])
+
+    
+
+
 if __name__ == "__main__":
     program = get_program("inputs/day-10-sample.txt")
     po = run_program(program=program)
-    # for r in po:
-    #     print(r)
     assert solution1(program_output=po) == 13140
+    solution2(program=program)
+
+    ########################################
 
     program = get_program("inputs/day-10-input.txt")
     po = run_program(program=program)
     assert solution1(program_output=po) == 12880
+    solution2(program=program)
