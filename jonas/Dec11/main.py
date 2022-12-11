b77d70ff3b119a3cea27e2a99593f57494a577e3
Jonas Bæk Precis <jonas@precisdigital.com>
2022-12-11 19:24:40 +0100

    cleaning

--- jonas/Dec11/main.py
+++ jonas/Dec11/main.py
@@ -111,21 +111,18 @@ def play_monkey_game(monkeys: list[Monkey], rounds: int, worry_level_divisor: in
 def get_n_most_active_monkeys(monkeys: list[Monkey], rounds: int, n: int, worry_level_divisor: int):
     monkeys_after_game = play_monkey_game(monkeys, rounds, worry_level_divisor)
 
-    #for monkey in monkeys_after_game: print(f"#{monkey.num}: {monkey.inspection_counter}")
-
     sorted_monkeys = sorted(monkeys_after_game, key=lambda x: x.inspection_counter, reverse=True)
     return sorted_monkeys[:n]
 
-# Sample 1 - 10605
-print(product([monkey.inspection_counter for monkey in get_n_most_active_monkeys(get_monkeys("sample_input.txt"), rounds=20, n=2, worry_level_divisor=3)]))
+def part_1_sample():
+    return product([monkey.inspection_counter for monkey in get_n_most_active_monkeys(get_monkeys("Dec11/sample_input.txt"), rounds=20, n=2, worry_level_divisor=3)])
 
-# Part 1 - 57838
-print(product([monkey.inspection_counter for monkey in get_n_most_active_monkeys(get_monkeys("input.txt"), rounds=20, n=2, worry_level_divisor=3)]))
-    
-# Sample 2 - 2713310158
-print(product([monkey.inspection_counter for monkey in get_n_most_active_monkeys(get_monkeys("sample_input.txt"), rounds=10000, n=2, worry_level_divisor=1)]))
+def part_1_answer():
+    return product([monkey.inspection_counter for monkey in get_n_most_active_monkeys(get_monkeys("Dec11/input.txt"), rounds=20, n=2, worry_level_divisor=3)])
 
-# Part 2 - 14399639972
-print(product([monkey.inspection_counter for monkey in get_n_most_active_monkeys(get_monkeys("input.txt"), rounds=10000, n=2, worry_level_divisor=1)]))
+def part_2_sample():
+    return product([monkey.inspection_counter for monkey in get_n_most_active_monkeys(get_monkeys("Dec11/sample_input.txt"), rounds=10000, n=2, worry_level_divisor=1)])
 
-
+def part_2_answer():
+    return product([monkey.inspection_counter for monkey in get_n_most_active_monkeys(get_monkeys("Dec11/input.txt"), rounds=10000, n=2, worry_level_divisor=1)])
+        
