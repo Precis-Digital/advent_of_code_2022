71f16b7be6edf355ffa174c504e50928a1dbcd89
pd-kaugesaar <kaugesaar@precisdigital.com>
2022-12-15 23:02:47 +0100

    Day 15 benchmark

--- kaug/benches/aoc_benchmark.rs
+++ kaug/benches/aoc_benchmark.rs
@@ -3,9 +3,10 @@ use kaug::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
 	let days = vec![
-		"day1", "day2", "day3", "day4", "day5",
-		"day6", "day7", "day8", "day9", "day10",
-		"day11", "day12", "day13", "day14"
+		//"day1", "day2", "day3", "day4", "day5",
+		//"day6", "day7", "day8", "day9", "day10",
+		//"day11", "day12", "day13", "day14"
+		"day15"
 	];
 	for day in days {
 		c.bench_function(format!("{}_part_1", &day).as_str(), |b| b.iter(|| {
